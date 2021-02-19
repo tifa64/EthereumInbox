f41adfa975a031796009b2701b4fd9ad891f4bff
Mostafa Ali Mansour <mostafa@oroboro.studio>
2021-02-19 20:23:46 +0100

    Adding Ganache, Mocha and web3 dependencies

---
+++ test/inbox.tst.js
@@ -0,0 +1,5 @@
+const assert = require("asserts");
+const ganache = require("ganache-cli");
+// Web3 is capital as we are requiring contructor and its convention
+// To have the constructor as capaital first letter
+const Web3 = require("web3");
