86304a9db647633911a4464268673018d13c519a
Mostafa Ali Mansour <mostafa@oroboro.studio>
2021-02-19 20:47:22 +0100

    Having POC test for deploying accounts

--- test/inbox.tst.js
+++ test/inbox.tst.js
@@ -1,5 +1,17 @@
-const assert = require("asserts");
+const assert = require("assert");
 const ganache = require("ganache-cli");
 // Web3 is capital as we are requiring contructor and its convention
 // To have the constructor as capaital first letter
 const Web3 = require("web3");
+const web3 = new Web3(ganache.provider());
+
+beforeEach(() => {
+  // Get a list of all accounts
+  web3.eth.getAccounts().then((fetchedAccounts) => {
+    console.log(fetchedAccounts);
+  });
+});
+
+describe("Inbox", () => {
+  it("deploys a contract", () => {});
+});
